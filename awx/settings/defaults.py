201507333723f201a350e826a326bdda5de44b45
Wayne Witzel III <wayne@riotousliving.com>
2017-11-09 15:36:13 -0500

    Fix supervisord and tz mistake

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -71,7 +71,6 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-USE_TZ = False
 TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
